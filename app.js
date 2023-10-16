7c8f7e0a11d45cc8d2e260094fdbb52aa377f019
POTHURAJU JAYAKRISHNA YADAV <132552104+jayakrishnayadav24@users.noreply.github.com>
2023-10-16 10:59:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'Doctor_Appointment'
+    user: 'jaya',
+    password: 'Jayakrishna24@',
+    database: 'doctor_appointment'
 });
 
 app.use(express.json());
